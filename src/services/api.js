00d986e34f84028c629aa8b90b15300a154544dd
Charlie Van Belle <r0995521@student.thomasmore.be>
2026-07-28 14:49:28 +0200

    fix: update path to api

--- src/services/api.js
+++ src/services/api.js
@@ -3,14 +3,14 @@ import axios from 'axios'
 const API_URL = 'https://api-b-j-challenge.onrender.com'
 
 export function login(email, password) {
-    return axios.post(`${API_URL}/api/v1/user/auth`, {
+    return axios.post(`${API_URL}/api/login`, {
         email,
         password
     })
 }
 
-export function getBags(token) {
-    return axios.get(`${API_URL}/api/v1/bag`, {
+export function getOrders(token) {
+    return axios.get(`${API_URL}/api/orders`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
