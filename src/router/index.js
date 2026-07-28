a0099c029a7078b6dcd8296e75d70b38cfd85d8c
Charlie Van Belle <r0995521@student.thomasmore.be>
2026-07-28 12:16:50 +0200

    fix: remove unneccesary import of view 'votes' from index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../vieuws/Login.vue'
 import Dashboard from '../vieuws/Dashboard.vue'
 import Detail from '../vieuws/Detail.vue'
-import Votes from '../vieuws/Votes.vue'
 
 const routes = [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
     { path: '/dashboard', component: Dashboard },
     { path: '/detail/:id', component: Detail },
-    { path: '/votes', component: Votes }
 ]
 
 export default createRouter({
