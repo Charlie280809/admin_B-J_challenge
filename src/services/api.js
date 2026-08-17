375f88a2c37d16b8491031c70a366374be54c13f
Charlie Van Belle <r0995521@student.thomasmore.be>
2026-08-17 19:30:43 +0200

    refactor: remove unused login function from API service

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,6 @@ import axios from 'axios'
 
 const API_URL = 'https://api-b-j-challenge.onrender.com'
 
-export function login(email, password) {
-    return axios.post(`${API_URL}/api/login`, {
-        email,
-        password
-    })
-}
-
 export function getOrders(token) {
     return axios.get(`${API_URL}/api/orders`, {
         headers: {
--- src/services/auth.js
+++ src/services/auth.js
@@ -34,5 +34,3 @@ export const useAuthStore = defineStore('auth', {
         }
     },
 });
-
-export default useAuthStore;
